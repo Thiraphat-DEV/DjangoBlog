e1aa02493634ec7684c25a546a1cd33092691fb4
thiraphat <630112418048@bru.ac.th>
2023-03-31 16:48:51 +0700

    add pillow& function photo

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Post
 # Register your models here.
+# control object with @ 
+@admin.register(Post)
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+	list_display = ['title', 'category', 'post_status','created_at','updated_at']
+	list_filter = ['category', 'post_status']
+	list_editable= ['category', 'post_status']
--- blog/models.py
+++ blog/models.py
@@ -3,8 +3,22 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
+	categories_choice =  (
+		("BlockChain", "BlockChain Technology"),
+		("AI", "Technology"),
+		("NFT", "NFT Technology"),
+		("BACKEND", "BACKEND Technology"),
+		("FRONTEND", "FRONTEND Technology"),
+	)
 	title = models.CharField(max_length=300)
 	body = models.TextField()
+	photo = models.ImageField(upload_to='blog/%Y/%m/%d', blank=True)
+	category = models.CharField(max_length=50, choices=categories_choice)
+	post_status = models.BooleanField(default=False)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
 
+	def Meta():
+		ordering = ['-created_at']
 	def __str__(self):
 		return self.title
