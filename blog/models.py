30fe5f59e72bee4169bb319dff198ebc457f5f6e
thiraphat <630112418048@bru.ac.th>
2023-03-31 15:34:19 +0700

    add blog to page

--- blog/models.py
+++ blog/models.py
@@ -3,4 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
-	pass
+	title = models.CharField(max_length=300)
+	body = models.TextField()
+
+	def __str__(self):
+		return self.title
