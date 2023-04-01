4951632f04968af38e1ad68e93d9e182949e0ee2
thiraphat <630112418048@bru.ac.th>
2023-04-01 21:08:33 +0700

    add paginators

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
-# Create your views here.
+from django.core.paginator import Paginator
 
 
 def index(request):
     posts = Post.objects.all()
-    return render(request, "index.html", {'posts': posts})
+    paginators = Paginator(posts, 50) # show 50 post as page
+    page_number = request.GET.get('page')
+    page_objs = paginators.get_page(page_number)
+    return render(request, "index.html", {'posts': page_objs})
 
 def post_detail(request, id):
     post = Post.objects.get(id=id)
